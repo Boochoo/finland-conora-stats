65e2d50c7f6922a2cf3fc13ff7091fab54d39ca1
Ermias Hailemicheal <ermias.hailemicheal@frantic.com>
2020-04-02 22:59:40 +0300

    Fixes null returns for lat and long from the world data Api

--- src/component/templates/WorldMap/WorldMap.js
+++ src/component/templates/WorldMap/WorldMap.js
@@ -32,6 +32,8 @@ class WorldMap extends Component {
     const { data } = this.props;
     const isSmallerScreen = window && window.innerWidth < 880;
 
+    const ddd = data.map((pos, index) => console.log(!pos.lat && pos));
+
     return (
       <MapContainer>
         <Map
@@ -45,53 +47,45 @@ class WorldMap extends Component {
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           />
-          {data.map(
-            (pos, index) =>
-              pos.lat &&
-              pos.long && (
-                <Marker
-                  key={index}
-                  draggable={false}
-                  position={[pos.lat, pos.long]}
-                >
-                  <CircleMarker
-                    center={[pos.lat, pos.long]}
-                    opacity={1}
-                    fillOpacity={0.5}
-                    fillColor={themeColors.red}
-                    color={themeColors.red}
-                    weight={2}
-                    radius={mapRadius(parseInt(pos.confirmed))}
-                    animate={true}
-                    onMouseOver={e => e.target.openPopup()}
-                    onMouseOut={e => e.target.closePopup()}
+          {data.map((pos, index) => (
+            <Marker
+              key={index}
+              draggable={false}
+              position={pos.lat && pos.long ? [pos.lat, pos.long] : [-1000, 0]}
+            >
+              <CircleMarker
+                center={pos.lat && pos.long ? [pos.lat, pos.long] : [-1000, 0]}
+                opacity={1}
+                fillOpacity={0.5}
+                fillColor={themeColors.red}
+                color={themeColors.red}
+                weight={2}
+                radius={mapRadius(parseInt(pos.confirmed))}
+                animate={true}
+                onMouseOver={e => e.target.openPopup()}
+                onMouseOut={e => e.target.closePopup()}
+              >
+                <Popup minWidth={50} offset={[-1, -3]} className='custom-popup'>
+                  <div
+                    style={{
+                      marginBottom: '0.25em'
+                    }}
                   >
-                    <Popup
-                      minWidth={50}
-                      offset={[-1, -3]}
-                      className='custom-popup'
-                    >
-                      <div
-                        style={{
-                          marginBottom: '0.25em'
-                        }}
-                      >
-                        <PTag popUpText={pos.countryRegion} isTitle />
-                        <PTag popUpText={`${pos.confirmed} confirmed`} />
-                        <PTag
-                          popUpText={`${pos.recovered} recovered`}
-                          color={themeColors.green}
-                        />
-                        <PTag
-                          popUpText={`${pos.deaths} deaths`}
-                          color={themeColors.red}
-                        />
-                      </div>
-                    </Popup>
-                  </CircleMarker>
-                </Marker>
-              )
-          )}
+                    <PTag popUpText={pos.countryRegion} isTitle />
+                    <PTag popUpText={`${pos.confirmed} confirmed`} />
+                    <PTag
+                      popUpText={`${pos.recovered} recovered`}
+                      color={themeColors.green}
+                    />
+                    <PTag
+                      popUpText={`${pos.deaths} deaths`}
+                      color={themeColors.red}
+                    />
+                  </div>
+                </Popup>
+              </CircleMarker>
+            </Marker>
+          ))}
         </Map>
       </MapContainer>
     );
